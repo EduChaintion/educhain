cf59e35774a6e8ca238365dd953e94e8997814a6
EduChaintion <89325469+EduChaintion@users.noreply.github.com>
2021-08-23 19:12:18 +1000

    Add files via upload

---
+++ script/contract.js
@@ -0,0 +1,211 @@
+const edu_coin_abi = {
+	abi: [
+		{
+			"inputs": [],
+			"stateMutability": "nonpayable",
+			"type": "constructor"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "",
+					"type": "address"
+				}
+			],
+			"name": "approvedInstitutions",
+			"outputs": [
+				{
+					"internalType": "string",
+					"name": "name",
+					"type": "string"
+				},
+				{
+					"internalType": "bool",
+					"name": "verified",
+					"type": "bool"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "uint256",
+					"name": "tokenId",
+					"type": "uint256"
+				}
+			],
+			"name": "burn",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "uint256",
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"name": "certificationNFTs",
+			"outputs": [
+				{
+					"internalType": "uint256",
+					"name": "tokenId",
+					"type": "uint256"
+				},
+				{
+					"internalType": "string",
+					"name": "image",
+					"type": "string"
+				},
+				{
+					"internalType": "address",
+					"name": "graduate",
+					"type": "address"
+				},
+				{
+					"internalType": "address",
+					"name": "institution",
+					"type": "address"
+				},
+				{
+					"internalType": "string",
+					"name": "certificationTitle",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "description",
+					"type": "string"
+				},
+				{
+					"internalType": "uint256",
+					"name": "date",
+					"type": "uint256"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "getAllCertification",
+			"outputs": [
+				{
+					"internalType": "uint256[]",
+					"name": "",
+					"type": "uint256[]"
+				},
+				{
+					"internalType": "string[]",
+					"name": "",
+					"type": "string[]"
+				},
+				{
+					"internalType": "address[]",
+					"name": "",
+					"type": "address[]"
+				},
+				{
+					"internalType": "string[]",
+					"name": "",
+					"type": "string[]"
+				},
+				{
+					"internalType": "string[]",
+					"name": "",
+					"type": "string[]"
+				},
+				{
+					"internalType": "uint256[]",
+					"name": "",
+					"type": "uint256[]"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "",
+					"type": "address"
+				}
+			],
+			"name": "graduateCertifications",
+			"outputs": [
+				{
+					"internalType": "uint256",
+					"name": "length",
+					"type": "uint256"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "to",
+					"type": "address"
+				},
+				{
+					"internalType": "string",
+					"name": "img",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "certTitle",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "desc",
+					"type": "string"
+				},
+				{
+					"internalType": "uint256",
+					"name": "date",
+					"type": "uint256"
+				}
+			],
+			"name": "mintNFT",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "tokenCount",
+			"outputs": [
+				{
+					"internalType": "uint256",
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "string",
+					"name": "name",
+					"type": "string"
+				}
+			],
+			"name": "verifyInstitution",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		}
+	]
+}
---
+++ script/script.js
@@ -0,0 +1,164 @@
+function loadMetaMask() {
+    let hasMetaMask = false;
+    if (window.ethereum) {
+        window.web3 = new Web3(window.web3.currentProvider);
+        // const web3 = window.web3;
+        window.ethereum.enable();
+        hasMetaMask = true;
+    }
+    return [window.web3, hasMetaMask];
+}
+
+const ret = loadMetaMask();
+const web3 = ret[0];
+const hasMetaMask = ret[1];
+
+let contract_instance;
+if(hasMetaMask) {
+    contract_instance = new web3.eth.Contract(edu_coin_abi.abi, edu_coin_addr, {}); // Load contract
+}
+
+function importAccount(account_private_key) {
+    return web3.eth.accounts.wallet.add('0x' + account_private_key);
+}
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        // Account Info
+        isUserLogged: false,
+        account_info: null,
+        account_private_key: '',
+        hasMetaMask: hasMetaMask,
+
+        // EduChaintion Info
+        email: "EduChaintion@outlook.com",
+        socials: [
+            { placeholder: "reddit", icon: "fa-brands:reddit-alien", link: "#" },
+            { placeholder: "twitter", icon: "bi:twitter", link: "#" },
+            { placeholder: "linkein", icon: "akar-icons:linkedin-fill", link: "#" },
+            { placeholder: "blog", icon: "fa-solid:blog", link: "#" }
+        ],
+        features: [
+            { icon: "eos-icons:blockchain", title: "Decentralisation", text: "Open, Free for everyone with a valid Ethereum address. How blockchain should be built for" },
+            { icon: "fluent:certificate-20-regular", title: "NFT certification", text: "Legititize your degree and qualification with valuable NFTs stored secure and immutable on Ethereum" },
+            { icon: "la:wallet", title: "Secure Wallet Setup", text: "Connect to Metamask to secure transactions and NFTs" },
+            { icon: "ri:coin-line", title: "Simple and Inexpensive", text: "Reduce processing time, complexity and save on fees and other cost" },
+        ],
+        steps: [
+            { step: 1, text: "Finish a course on EduChaintion, online, or in person" },
+            { step: 2, text: "The respected academic institution will have the ability to mint a NFT if qualified" },
+            { step: 3, text: "NFT is sent to you to keep and accessible through API or our App" },
+        ],
+
+        // Data
+        verifyInstitutionParams: { name: "" },
+        createNftParams: { 
+            image: "", description: "",
+            graduate: "",
+            certification_title: "", date: "" },
+
+        yourNFTs: [],
+        statusMsg: "",
+
+        nftInfo: { name: "", symbol: "" },
+        ethPrice: 0,
+
+        proofOfConceptData: {
+            level: ["Bachelor", "Master", "Doctoral", "Diploma", "Certification"],
+            subject: ["Engineering", "Art & Design", "Medicine & Health", "Business", "Science", "Computer Science", "Commerce", "Law"],
+            description: ["Hereby awards this certification for excellency", "The university has granted you with this award", "Sufficiently completed all accredit courses"]
+        },
+    },
+    mounted: async function () {
+        this.onload();
+    },
+    methods: {
+        onload: async function () {
+            if (this.hasMetaMask) {
+                await this.loadMetaMaskAccount();
+                // console.log(`User Account: ${this.account_info}`);
+                this.isUserLogged = true;
+            }
+
+            await this.loadEthereumPrice();
+            await this.loadUserCertification();
+        },
+        onImportAccount: async function () {
+            this.account_info = importAccount(this.account_private_key);
+            this.isUserLogged = true;
+            console.log("Login Success!");
+        },
+        onCreateAccount: function () {
+            createAccount();
+            // this.account_info = account_info.address;
+        },
+        createNFT: async function () {
+            if(!web3.utils.isAddress(this.createNftParams.graduate)) { this.statusMsg = "Invalid Address"; return; }
+            
+            // this.createNftParams.institution = this.account_info.address;
+            this.createNftParams.date = Date.now();
+
+            let _args = [
+                this.createNftParams.graduate,
+                this.createNftParams.image,
+                this.createNftParams.certification_title,
+                this.createNftParams.description,
+                this.createNftParams.date,
+            ]
+            console.log(_args);
+            this.statusMsg = await methodSend(web3, this.account_info, contract_instance, 'send', 'mintNFT', _args);
+        },
+        generateNFT: async function () {
+            var type = this.proofOfConceptData.level[Math.floor(Math.random()*this.proofOfConceptData.level.length)];
+            var title = this.proofOfConceptData.subject[Math.floor(Math.random()*this.proofOfConceptData.subject.length)];
+            this.createNftParams.certification_title = `${type} of ${title}`;
+            this.createNftParams.description = this.proofOfConceptData.description[Math.floor(Math.random()*this.proofOfConceptData.description.length)];
+            this.createNftParams.graduate = (this.account_info) ? `${this.account_info.address}` : "An ethereum address" ;
+            this.createNftParams.image = 'https://dummyimage.com/640x360/fff/aaa';
+        },
+        verifyInstitution: async function () {
+            try {
+                this.statusMsg = "Processing to blockchain ...";
+                this.statusMsg = await methodSend(web3, this.account_info, contract_instance, 'send', 'verifyInstitution', [this.verifyInstitutionParams.name]);
+            } catch (error) {
+                this.statusMsg = error;
+            }
+            console.log(this.statusMsg);
+        },
+        emailForEnquiry: async function() {
+            console.log("Sending Email");
+        },
+        loadMetaMaskAccount: async function () {
+            window.ethereum.request({ method: 'eth_requestAccounts' })
+            .then((accounts) => {
+                this.account_info = { address: accounts[0] };
+                this.isUserRegistered = true;
+            });
+        },
+        loadUserCertification: async function() {
+            let ret = await methodSend(web3, this.account_info, contract_instance, 'call', 'getAllCertification', []);
+
+            this.yourNFTs = [];
+            let tokenIds = ret[0]; let images = ret[1]; let institutions = ret[2]; 
+            let certTitles = ret[3]; let descs = ret[4]; let dates = ret[5];
+
+            for(let i in tokenIds) {
+                this.yourNFTs.push({
+                    tokenId: tokenIds[i], image: images[i], institution: institutions[i],
+                    certification_title: certTitles[i], description: descs[i], date: parseInt(dates[i])
+                });
+            }
+        },
+        loadEthereumPrice: async function () {
+            let json = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd').then(response => response.json())
+            this.ethPrice = json.ethereum.usd;
+        }
+    },
+    watch: {
+        tokenId: async function (value) {
+            console.log(value);
+        },
+
+    },
+})
